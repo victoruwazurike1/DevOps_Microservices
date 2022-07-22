e4e6d876e6eca4a62b9e315988efa4a7a000c10f
Uwazurike Victor <victoru779@gmail.com>
2022-07-22 22:08:49 +0100

    modified files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=paxtor2/machine-learning-microservice:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+# kubectl create deploy machine-learning-microservice-kube --image=$dockerpath
+kubectl run udacity-api-pod --image=$dockerpath --port=80 --labels app=udacity-api-pod
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+# kubectl port-forward pod/machine-learning-microservice-kube-5b9f8587b-knbt4 --address 0.0.0.0 8000:80
+kubectl port-forward udacity-api-pod 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=paxtor2/machine-learning-microservice:latest
 
 # Step 2:  
 # Authenticate & tag
+docker login --username paxtor2
+docker tag machine-learning-microservice $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
